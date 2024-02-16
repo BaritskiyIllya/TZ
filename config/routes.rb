d605fc1039120b9d0d3df3f6e8b7485798fa4ffc
BaritskiyIllya <baritskiy.illya@gmail.com.com>
2024-02-16 16:50:02 +0200

    Add active admin and setup resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  ActiveAdmin.routes(self)
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       get '/', to: 'device_experiments#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-
-experiment_options_btn = [{ experiment_id: 1, option: { btn_color: "#ff0000" }, max_percentage: 33.3 }, { experiment_id: 1, option: { btn_color: "#0000ff" }, max_percentage: 33.3 }, { experiment_id: 1, option: { btn_color: "#00ff00" }, max_percentage: 33.3 }]
-experiment_options_price = [{ experiment_id: 2, option: { price: "10" }, max_percentage: 75 }, { experiment_id: 2, option: { price: "20" }, max_percentage: 10 }, { experiment_id: 2, option: { price: "50" }, max_percentage: 5 }, { experiment_id: 2, option: { price: "5" }, max_percentage: 10 }]
-experiment_names = [{ name: 'btn_color' }, { name: 'price' }]
-
-Experiment.create!(experiment_names)
-ExperimentsOption.create!(experiment_options_btn)
-ExperimentsOption.create!(experiment_options_price)
-
+#
+# experiment_options_btn = [{ experiment_id: 1, option: { btn_color: "#ff0000" }, max_percentage: 33.3 }, { experiment_id: 1, option: { btn_color: "#0000ff" }, max_percentage: 33.3 }, { experiment_id: 1, option: { btn_color: "#00ff00" }, max_percentage: 33.3 }]
+# experiment_options_price = [{ experiment_id: 2, option: { price: "10" }, max_percentage: 75 }, { experiment_id: 2, option: { price: "20" }, max_percentage: 10 }, { experiment_id: 2, option: { price: "50" }, max_percentage: 5 }, { experiment_id: 2, option: { price: "5" }, max_percentage: 10 }]
+# experiment_names = [{ name: 'btn_color' }, { name: 'price' }]
+#
+# Experiment.create!(experiment_names)
+# ExperimentsOption.create!(experiment_options_btn)
+# ExperimentsOption.create!(experiment_options_price)
+#
